e2d37e8dc42aa41a923edb6c629b5aaf5127bcac
Chema23x <jmanuel.hernandezb23@gmail.com>
2024-10-20 20:55:56 -0600

    First commit at Puppeteer project

---
+++ src/pages/api/puppeteer.ts
@@ -0,0 +1,39 @@
+import puppeteer from 'puppeteer';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 200,
+        });
+
+        const context = await browser.createBrowserContext();
+
+        const page1 = await context.newPage();
+        const page2 = await context.newPage();
+
+        await page1.goto('https://www.mercadolibre.com.mx/#from=homecom');
+        await page2.goto('https://google.com')
+        // Set screen size.
+        await page1.setViewport({ width: 1080, height: 1024 });
+        // Esperar hasta que el input esté disponible en el DOM
+        await page1.waitForSelector('#cb1-edit');
+
+        // Hacer clic en el input
+        const inputSelector = '#cb1-edit'; // Cambia esto al selector correcto del input en tu página
+        await page1.click(inputSelector);
+
+        // Escribir "hola" en el input
+        await page1.type(inputSelector, 'Xbox series X');
+
+        // Opcionalmente: hacer clic en el botón para enviar el formulario
+        const submitButtonSelector = '.nav-search-btn';
+        await page1.click(submitButtonSelector);
+
+
+        res.status(200).json({ message: 'Page opened successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to open the page' });
+    }
+}
